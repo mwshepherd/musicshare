03837b1fc4bdc1a80c6612a41523496b4813e512
mwshepherd <mwshepherd11@gmail.com>
2020-05-12 12:01:19 +1000

    added listings model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { in: 1...16 }
   validates :fullname, presence: true, length: { in: 1...71 }
   validates :bio, allow_nil: true, length: { in: 1...251 }
+  has_many :listings, dependent: :destroy
 end
