936a2824f28dc1ffb4be55d4c5d0c5549cd2bb51
mwshepherd <mwshepherd11@gmail.com>
2020-05-12 11:45:44 +1000

    user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, length: { in: 1...15 }
+  validates :username, presence: true, length: { in: 1...16 }
+  validates :fullname, presence: true, length: { in: 1...71 }
+  validates :bio, allow_nil: true, length: { in: 1...251 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,48 @@ RSpec.describe User, type: :model do
       expect(subject).to be_valid
     end
 
+    it 'is not valid without a full name' do
+      subject.fullname = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid with a fullname up to 70 characters' do
+      subject.fullname = 'a' * 70
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid with a fullname greater than 70 characters' do
+      subject.fullname = 'a' * 71
+      expect(subject).to_not be_valid
+    end
+
     it 'is not valid without a username' do
       subject.username = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid with a username up to 15 characters' do
+      subject.username = 'a' * 15
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid with a username greater than 15 characters' do
+      subject.username = 'a' * 16
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid without a bio' do
+      subject.bio = nil
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid with a bio greater than 250 characters' do
+      subject.bio = 'a' * 251
       expect(subject).to_not be_valid
     end
   end
