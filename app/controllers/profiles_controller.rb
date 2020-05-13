46ea04d246f4624675481e0f0c78668d37d8e388
mwshepherd <mwshepherd11@gmail.com>
2020-05-13 16:06:58 +1000

    user profile picture, listing-card

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,10 @@ class ProfilesController < ApplicationController
     def index
         @user = User.where(username: params[:username])[0]
     end
+
+    def edit
+    end
+
+    def update
+    end
 end
