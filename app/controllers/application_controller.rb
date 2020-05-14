1eef2494e7e7323ea180a9a62effa261069616f7
mwshepherd <mwshepherd11@gmail.com>
2020-05-14 11:16:41 +1000

    added search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
             user_params.permit(:fullname, :username, :email, :picture, :password, :password_confirmation)
         end
         devise_parameter_sanitizer.permit(:account_update) do |user_params|
-            user_params.permit(:fullname, :username, :email, :picture, :password, :password_confirmation, :current_password)
+            user_params.permit(:fullname, :username, :email, :bio, :picture, :password, :password_confirmation, :current_password)
         end
     end
 end
