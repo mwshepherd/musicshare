de5dd81dae1a5c986f0b72946c8afc86ced13723
mwshepherd <mwshepherd11@gmail.com>
2020-06-13 16:27:22 +1000

    find average score

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,8 @@ class ProfilesController < ApplicationController
     def index
         # Will find the user based upon the username in the query string
         @user = User.where(username: params[:username])[0]
+        @rating = get_rating(@user)
+
     end
 
     def listings
@@ -14,4 +16,19 @@ class ProfilesController < ApplicationController
         @orders = current_user.orders
     end
 
+    private
+
+    def get_rating(user)
+        sum = 0
+
+        if user.ratings.length < 1
+            return 'No Ratings Yet'
+        else
+            user.ratings.each do |rating|
+                sum += rating.score
+            end
+
+            return (sum.to_f / user.ratings.length.to_f).round(2)
+        end
+    end
 end
